e02461f14eb3dbe020be05bae69aa08cc302de33
edwardnovrizal <edwardnovrizal@gmail.com>
2024-09-17 13:14:30 +0700

    mengganti respone score untuk client

--- src/controllers/client/read/client_score.js
+++ src/controllers/client/read/client_score.js
@@ -1,11 +1,14 @@
+const JawabanModel = require("../../../models/jawaban");
 const ScoreModel = require("../../../models/score");
+const SoalModel = require("../../../models/soal");
 
 const ReadClientScore = async (req, res) => {
   const { id_mapel } = req.query;
   const { id_murid } = req.murid;
 
   try {
-    const Respone = await ScoreModel.findOne({ mapel: id_mapel, murid: id_murid });
+    const jumlahSoal = await SoalModel.countDocuments({ id_mapel });
+    const Respone = await JawabanModel.findOne({ mapel: id_mapel, murid: id_murid });
 
     if (Respone == null) {
       return res.status(404).send({
@@ -13,11 +16,14 @@ const ReadClientScore = async (req, res) => {
         message: "Data Score Tidak Ditemukan",
       });
     }
-
+    const result = {
+      nilai: JawabanModel.skor_total,
+      jumlah_soal: jumlahSoal,
+    };
     return res.status(200).send({
       code: res.statusCode,
       message: "Berhasil Get Data Score",
-      data: Respone,
+      data: result,
     });
   } catch (error) {
     return res.status(400).send({
